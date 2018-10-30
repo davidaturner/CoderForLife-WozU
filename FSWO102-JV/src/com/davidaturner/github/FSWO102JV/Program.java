a6c8d79dceec36db9b8f18b0295050a8ab76b5f8
turnin19@earthlink.net <turnin19@earthlink.net>
2018-10-30 15:52:32 -0400

    Implement Lesson4

--- FSWO102-JV/src/com/davidaturner/github/FSWO102JV/Program.java
+++ FSWO102-JV/src/com/davidaturner/github/FSWO102JV/Program.java
@@ -3,27 +3,23 @@ package com.davidaturner.github.FSWO102JV;
 import com.davidaturner.github.FSWO102JV.Lesson1.HandsOn1;
 import com.davidaturner.github.FSWO102JV.Lesson2.HandsOn2;
 import com.davidaturner.github.FSWO102JV.Lesson3.HandsOn3;
+import com.davidaturner.github.FSWO102JV.Lesson4.HandsOn4;
 
 public class Program {
 
 	
 	public static void main(String[] args) {
 		
-		HandsOn3 lesson = new HandsOn3 ();
-
 		System.out.println("Running HandsOn Exercises");
 		System.out.println();
 		
-		System.out.println("Step 1");
-		lesson.Step1();
-		System.out.println();
+		HandsOn4 lesson = new HandsOn4 ();
 		
-		System.out.println("Step 2");
+		lesson.Step1();	
 		lesson.Step2();
-		System.out.println();	
-		
-		System.out.println("Step 3");
 		lesson.Step3();	
+
+		System.out.println("DONE!");
 		System.out.println();
 	}
 
