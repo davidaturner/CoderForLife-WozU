391faf8253460e1928d8fa1634340b62a20b16c0
turnin19 <turnin19@earthlink.net>
2018-12-04 22:49:13 -0500

    Sample javascript hands on.

--- FSWO101/FSWO101/Lesson 1/script.js
+++ FSWO101/FSWO101/Lesson 1/script.js
@@ -1,2 +1,21 @@
-﻿
-alert("Hello, Brutal World!")
+﻿let age = 16;
+
+function allowedIntoMovie() {
+    if (age < 0) {
+        console.log("Age " + age + " is invalid.");
+    }
+    else if (age >= 0 && age < 10) {
+        console.log("Not permitted");
+    }
+    else if (age < 15) {
+        console.log("Permitted with parent");
+    }
+    else if (age < 18) {
+        console.log("Permitted with anyone over 18");
+    }
+    else {
+        console.log("Permitted to attend alone.");
+    }
+}
+
+allowedIntoMovie();
