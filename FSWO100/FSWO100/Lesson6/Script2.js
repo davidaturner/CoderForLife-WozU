595f646b9ab939e6eeaca954a81324b60e006c54
turnin19 <turnin19@earthlink.net>
2019-03-31 13:25:53 -0400

    Completed Lesson 6

---
+++ FSWO100/FSWO100/Lesson6/Script2.js
@@ -0,0 +1,10 @@
+// alert('Welcome to my Page!');
+var myFavoriteNumber = 42;
+var myFavoritePet = "Dawg";
+var rainIsLovely = true;
+
+var numberOne = 24;
+var numberTwo = 57;
+
+var addNumbers = numberOne + numberTwo;
+var divideNumbers = numberOne / numberTwo;
