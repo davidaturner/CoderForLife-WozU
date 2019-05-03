70e33a517652d97542e544ef37c2330b00f89aab
turnin19 <turnin19@earthlink.net>
2019-05-02 22:20:06 -0400

    Completed Lesson 7

---
+++ FSWO101/FSWO101/Lesson 7/Script.js
@@ -0,0 +1,22 @@
+// alert('Hello World from JS');
+function regexMe() {
+
+    var firstname = document.getElementById("firstname");
+    var lastname = document.getElementById("lastname");
+
+    console.log(firstname.value);
+    console.log(lastname.value);
+
+    let nameRegex = "^[A-Z][a-zA-Z ]*";
+
+    if (firstname.value.match(nameRegex) &&
+        lastname.value.match(nameRegex)) {
+        console.log(true);
+        alert("Yay! Your inputs were all correct!");
+
+    }
+    else {
+        console.log(false);
+        alert("Oh no! Thats an invalid format!");
+    }
+}
