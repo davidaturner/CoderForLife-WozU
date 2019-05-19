91b4a7dce904b918b25ff7611f7651d599fc401f
turnin19 <turnin19@earthlink.net>
2019-05-18 22:40:56 -0400

    Completed Lesson 6.

---
+++ FSWO102-CS/Lesson6/Activity/Activity/Turnerstuff/FinalExample.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// More class and class methods found in Turnerstuff folder.
+
+namespace Activity
+{
+    // new nested namespace
+    namespace NewNestedNamespace
+    {
+        class MyExample
+        {
+            public static void Print()
+            {
+                Console.WriteLine("Calling newnestednamespace member.");
+            }
+        }
+    }
+
+    class MyPersonal
+    {
+        public static void Print()
+        {
+            Console.WriteLine("Calling mypersonal namespace member..");
+        }
+    }
+}
+namespace Activity.NestedNamespace
+{
+    class FinalExample
+    {
+        public static void Print()
+        {
+            Console.WriteLine("Calling finalexample nestednamespace member.");
+        }
+    }
+}
+
+namespace PrivateNamespace
+{
+    class MyExample
+    {
+        public static void Print()
+        {
+            Console.WriteLine("Calling private namespace member.");
+        }
+    }
+}
+
---
+++ FSWO102-CS/Lesson6/HandsOnL06/HandsOnL06/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using mine = myNamespace.AnotherProgram;
+using joey = joeyNamespace.AnotherProgram;
+using textNamespace;
+
+namespace HandsOnL06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            joey.PrintText();
+            mine.PrintText();
+            AnotherProgram.PrintText(); // from textNamespace. vague, when no use alias.
+
+            Console.WriteLine("\r\nDONE!");
+            Console.ReadLine();
+        }
+    }
+}
