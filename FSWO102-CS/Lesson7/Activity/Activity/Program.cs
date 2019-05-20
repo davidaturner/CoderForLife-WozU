1bbdb3302e57a62cafbd821cd27f71815e21b419
turnin19 <turnin19@earthlink.net>
2019-05-20 15:56:48 -0400

    Completed Lesson 7. 102CS.

---
+++ FSWO102-CS/Lesson7/Activity/Activity/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity
+{
+    class Program
+    {
+        static public void GiveBirth(Animal animal)
+        {
+            Console.WriteLine(animal.GiveBirth());
+        }
+        static void Main(string[] args)
+        {
+            // Subtyping Polymorphism.
+            GiveBirth(new Animal());
+            GiveBirth(new Mammal());
+            GiveBirth(new Bird());
+            Console.WriteLine();
+
+            // Ad hoc polymorphism.
+            Animal test = new Animal();
+            Console.WriteLine(test.GiveBirth());
+            Console.WriteLine(test.GiveBirth(2));
+
+            Console.WriteLine("\r\nDONE!");
+        }
+    }
+
+    public class Animal
+    {
+        public virtual string GiveBirth()
+        {
+            return "I don't know how";
+        }
+        public string GiveBirth(int tries)
+        {
+            return "I can give birth at least " + tries + " times a year.";
+        }
+    }
+
+    public class Mammal : Animal
+    {
+        public override string GiveBirth()
+        {
+            return "I give live birth";
+        }
+    }
+
+    public class Bird: Animal
+    {
+        public override string GiveBirth()
+        {
+            return "I lay eggs";
+        }
+    }
+}
