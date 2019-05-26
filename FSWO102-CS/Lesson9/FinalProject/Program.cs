6dddb88d0dc6aef0c0eb5e81d3d396e18ec55637
turnin19 <turnin19@earthlink.net>
2019-05-26 15:55:31 -0400

    Complete Final Project.

--- FSWO102-CS/Lesson9/FinalProject/Program.cs
+++ FSWO102-CS/Lesson9/FinalProject/Program.cs
@@ -8,9 +8,47 @@ namespace FinalProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World from Program!");
-            BlingBlong.Run();
-            Sequence.Run();
+            RunPart1();
+            Console.WriteLine();
+            RunPart2();
+            Console.WriteLine("\r\nDONE!");
+            Console.WriteLine();
+        }
+
+        static public void RunPart1() 
+        {
+            // ASSUMPTION: Using only non negative integers as input.
+
+            // TESTCASE: Test one step: value=15
+            // TESTCASE: Test one step: value=5
+            // TESTCASE: Test one step: value=3
+            // TESTCASE: Test one step: value=1
+            // TESTCASE: Test one step: value=2
+            // TESTCASE: Test one step: value=45
+            // TESTCASE: Test one step: value=75
+            // TESTCASE: Test one step: value=10
+            // TESTCASE: Test one step: value=48
+            // TESTCASE: Test one step: value=225
+            // TESTCASE: Test one step: value=100
+            // BlingBlong.Test();
+
+            // TESTCASE: Run first 100 steps.
+            BlingBlong.Run(100);
+        }
+
+        static public void RunPart2()
+        {
+            // ASSUMPTION: Use only non negative integer values.
+
+            // TESTCASE: Test one step: value=0
+            // TESTCASE: Test one step: value=1
+            // TESTCASE: Test one step: value=2
+            // TESTCASE: Test one step: value=3
+            // TESTCASE: Test one step: value=10
+            // Sequence.Test();
+
+            // TESTCASE: Run 0, 1, 2 and 8 additional steps.
+            Sequence.Run(8);
         }
     }
 }
--- FSWO102-CS/Lesson9/FinalProject/Sequence.cs
+++ FSWO102-CS/Lesson9/FinalProject/Sequence.cs
@@ -5,9 +5,40 @@ namespace FinalProject.Part2
     public class Sequence
     {
 
-        static public void Run()
+        static public void Run(int limit)
         {
-            Console.WriteLine("Hello World from Sequence!");
+            for(int i=0; i<limit+3; i++)
+            {
+                Console.WriteLine( DetermineFibonacci(i));
+            }
+        }
+
+        static public void Test()
+        {
+            // TESTCASE: Test one step: value=0
+            // TESTCASE: Test one step: value=1
+            // TESTCASE: Test one step: value=2
+            // TESTCASE: Test one step: value=3
+            // TESTCASE: Test one step: value=10
+            Console.WriteLine(DetermineFibonacci(0) + " 0");    // 0
+            Console.WriteLine(DetermineFibonacci(1) + " 1");    // 1
+            Console.WriteLine(DetermineFibonacci(2) + " 1");    // 1
+            Console.WriteLine(DetermineFibonacci(3) + " 2");    // 2
+            Console.WriteLine(DetermineFibonacci(4) + " 3");    // 3
+            Console.WriteLine(DetermineFibonacci(12) + " 144");  // 144
+
+        }
+
+        static public int DetermineFibonacci(int i)
+        {
+            // Base case.
+            if (i<2)
+            {
+                return i;
+            }
+
+            // Recursive case.
+            return DetermineFibonacci(i - 2) + DetermineFibonacci(i - 1);
         }
     }
 }
