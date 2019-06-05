3e02ff4935b90376b359b15ca81f48592976781a
turnin19 <turnin19@earthlink.net>
2019-06-05 15:13:00 -0400

    Completing Final Project 100.

--- FSWO100/LessonA/Project/script.js
+++ FSWO100/LessonA/Project/script.js
@@ -3,13 +3,51 @@ function idCard() {
 
     var firstName = document.getElementById('firstname').value;
     var lastName = document.getElementById('lastname').value;
+
+    var postFullName = document.getElementById('postFullName');
+    postFullName.innerHTML = firstName + " " + lastName;
+    if (postFullName.innerHTML < 2) {
+        postFullName.innerHTML = "Your Name";
+    }
+
+    var address = document.getElementById('address').value;
+    postAddress.innerHTML = "<em>" + address + "</em>";
+
     var age = document.getElementById('age').value;
     var phone = document.getElementById('phone').value;
-    var address = document.getElementById('address').value;
 
-    console.log(firstName);
-    console.log(lastName);
-    console.log(age);
-    console.log(phone);
-    console.log(address);
+    var numberArray = [];
+    numberArray.push(age);
+    numberArray.push(phone);
+    numberArray.forEach(function (element) {
+        var elementstr = "<em>" + element + "</em>";
+        if (element <= 100) {
+            document.getElementById('postAge').innerHTML = elementstr;
+        }
+        else {
+            document.getElementById('postPhone').innerHTML = elementstr;
+        }
+    });
+
+    document.getElementById('idcard').hidden = false;
 }
+
+function reset() {
+
+    document.getElementById("idcard").hidden = true;
+
+    document.getElementById('firstname').value = "";
+    document.getElementById('lastname').value = "";
+    document.getElementById('address').value = "";
+    document.getElementById('age').value = "";
+    document.getElementById('phone').value = " ";
+
+    document.getElementById('postFullName').innerHTML = "";
+    document.getElementById('postAddress').innerHTML = "";
+    document.getElementById('postAge').innerHTML = "";
+    document.getElementById('postPhone').innerHTML = "";
+
+}
+
+reset();
+
