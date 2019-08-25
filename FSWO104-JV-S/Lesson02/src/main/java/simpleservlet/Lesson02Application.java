793e2510c6418230c8dd5bde38c4a25ccf9ff1ca
turnin19 <turnin19@earthlink.net>
2019-08-25 12:47:59 -0400

    Finally got it! Three projects for git!

---
+++ FSWO104-JV-S/Lesson02/src/main/java/simpleservlet/Lesson02Application.java
@@ -0,0 +1,13 @@
+package simpleservlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lesson02Application.class, args);
+	}
+
+}
