e62aeee27dc5c06bc2e5c7b95c7de16abeaddd21
turnin19 <turnin19@earthlink.net>
2019-08-25 12:10:13 -0400

    Still working on it.

---
+++ FSWO104-JV-S/Lesson02/src/main/java/com/lesson02/simpleservlet/SimpleServletApplication.java
@@ -0,0 +1,15 @@
+package com.lesson02.simpleservlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan
+@SpringBootApplication
+public class SimpleServletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleServletApplication.class, args);
+	}
+
+}
