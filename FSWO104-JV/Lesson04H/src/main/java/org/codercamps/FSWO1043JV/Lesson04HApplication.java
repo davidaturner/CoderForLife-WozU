02a5ee8bec472711a5114fd38d344893885fb796
turnin19 <turnin19@earthlink.net>
2019-10-14 22:18:17 -0400

    Added changes to Lesson04

---
+++ FSWO104-JV/Lesson04H/src/main/java/org/codercamps/FSWO1043JV/Lesson04HApplication.java
@@ -0,0 +1,13 @@
+package org.codercamps.FSWO1043JV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson04HApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lesson04HApplication.class, args);
+	}
+
+}
