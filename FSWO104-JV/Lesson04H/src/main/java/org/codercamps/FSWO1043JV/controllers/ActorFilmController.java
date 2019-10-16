7bfd40206668cddcc68d576968a558e397852ac1
turnin19 <turnin19@earthlink.net>
2019-10-16 13:15:35 -0400

    Change destination for Lesson04 HandsOn.

--- FSWO104-JV/Lesson04H/src/main/java/org/codercamps/FSWO1043JV/controllers/ActorFilmController.java
+++ FSWO104-JV/Lesson04H/src/main/java/org/codercamps/FSWO1043JV/controllers/ActorFilmController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping({"/", "/actorfilms"})
+@RequestMapping({"/", "/actors"})
 public class ActorFilmController {
 
 	@Value("${spring.datasource.url}")
