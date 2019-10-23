c80953644d24fdafff2cb58b383a388d33df8990
turnin19 <turnin19@earthlink.net>
2019-10-23 10:09:55 -0400

    Completed optional HandsOnL05.

--- FSWO104-JV/Lesson05/src/main/java/com/dogo/chat/MessageController.java
+++ FSWO104-JV/Lesson05/src/main/java/com/dogo/chat/MessageController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,9 +39,20 @@ public class MessageController {
 		Message createdMessage = dao.save(message);
 		return ResponseEntity.ok(createdMessage);
 	}
+
+	@PutMapping("/chat")
+	public ResponseEntity<Message> putMessage(@RequestBody Message message) {
+		Message foundMessage = dao.findById(message.getId()).orElse(null);
+		if (foundMessage == null) {
+			return ResponseEntity.notFound().header("Message", "Nothing found with that id").build();
+		} 
+			
+		dao.save(message);
+		return ResponseEntity.ok().build();
+	}
 	
 	@DeleteMapping("/chat/{id}")
-	public ResponseEntity<Message>deleteMessage(@PathVariable(value="id") Integer id) {
+	public ResponseEntity<Message> deleteMessage(@PathVariable(value="id") Integer id) {
 		Message foundMessage = dao.findById(id).orElse(null);
 		if (foundMessage == null) {
 			return ResponseEntity.notFound().header("Message", "Nothing found with that id").build();
