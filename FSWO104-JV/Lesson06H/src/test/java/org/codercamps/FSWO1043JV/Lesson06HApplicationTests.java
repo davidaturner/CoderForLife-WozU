298742d4af7336f4f893807d7a3a5c82be1e49e0
turnin19 <turnin19@earthlink.net>
2019-11-07 18:09:59 -0500

    Worked on Lesson06 HandsOn

---
+++ FSWO104-JV/Lesson06H/src/test/java/org/codercamps/FSWO1043JV/Lesson06HApplicationTests.java
@@ -0,0 +1,13 @@
+package org.codercamps.FSWO1043JV;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson06HApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
