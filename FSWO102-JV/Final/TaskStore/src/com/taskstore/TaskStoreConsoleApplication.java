3e802c8d3f5481f0a45def7de53cd3ca90167011
turnin19 <turnin19@earthlink.net>
2019-12-13 21:52:33 -0500

    Update 104JV Final for increased testability.

--- FSWO102-JV/Final/TaskStore/src/com/taskstore/TaskStoreConsoleApplication.java
+++ FSWO102-JV/Final/TaskStore/src/com/taskstore/TaskStoreConsoleApplication.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 
 import com.taskstore.classes.CompleteableTask;
 import com.taskstore.classes.SimpleTask;
+import com.taskstore.interfaces.ICompleteable;
 import com.taskstore.interfaces.ITaskable;
 
 public class TaskStoreConsoleApplication {
@@ -13,21 +14,26 @@ public class TaskStoreConsoleApplication {
 	private static int nextTaskNumber = 0;
 	
 	private static ArrayList<ITaskable>mainTasks;
-	private static ArrayList<ITaskable>completeableTasks;
+	private static ArrayList<ICompleteable>completeableTasks;
 	
 	public static BufferedReader reader;
 	
-	public static int ERROR_SELECT = -1;
-	public static int QUIT_SELECT = 5;
+	public static final int ERROR_SELECT = -1;
+	public static final int NO_SINGLE_OP = 0;
+	public static final int ADD_SELECT = 1;
+	public static final int REMOVE_SELECT = 2;
+	public static final int COMPLETE_SELECT = 3;
+	public static final int LIST_SELECT = 4;
+	public static final int QUIT_SELECT = 5;
 	
 	public static String NEWLINE = "\r\n";
 	
 	public static String MAIN_PROMPT = NEWLINE + "What would you like to do?";
-	public static String ADD_PROMPT = NEWLINE + "Enter a task";
-	public static String CONTINUE_PROMPT = NEWLINE + "Press Return to Continue";
-	public static String SELECT_PROMPT = NEWLINE + "Select a task (by number)";
-	public static String QUIT_PROMPT = NEWLINE + "Quitting application...";
-	public static String NOT_RECOGNIZED_PROMPT = NEWLINE + "Choice Not Recognized. Try Again?";
+	public static String ADD_PROMPT = "Enter a task";
+	public static String CONTINUE_PROMPT = "Press Return to Continue";
+	public static String SELECT_PROMPT = "Select a task (by number)";
+	public static String QUIT_PROMPT = "Quitting application...";
+	public static String NOT_RECOGNIZED_PROMPT = "Choice Not Recognized. Try Again?";
 		
 	public static String MAIN_PAGE = NEWLINE + "MAIN PAGE";
 	public static String ADD_PAGE = NEWLINE + "ADD PAGE";
@@ -37,16 +43,32 @@ public class TaskStoreConsoleApplication {
 	
 	public TaskStoreConsoleApplication () {
 		mainTasks = new ArrayList<ITaskable>();
-		mainTasks.add( new SimpleTask("1", "Add a Task"));
-		mainTasks.add( new SimpleTask("2", "Remove a Task"));
-		mainTasks.add( new SimpleTask("3", "Mark Task as Complete"));
-		mainTasks.add( new SimpleTask("4", "List Tasks"));
-		mainTasks.add( new SimpleTask("5", "Quit"));
+		mainTasks.add( new SimpleTask("" + ADD_SELECT, "Add a Task"));
+		mainTasks.add( new SimpleTask("" + REMOVE_SELECT, "Remove a Task"));
+		mainTasks.add( new SimpleTask("" + COMPLETE_SELECT, "Mark Task as Complete"));
+		mainTasks.add( new SimpleTask("" + LIST_SELECT, "List Tasks"));
+		mainTasks.add( new SimpleTask("" + QUIT_SELECT, "Quit"));
 		
-		completeableTasks = new ArrayList<ITaskable>();
+		completeableTasks = new ArrayList<ICompleteable>();
 	}
 	
+	public void loadSimpleTasks(String[] tasks) {
+		for(int i=0; i<tasks.length;i++) {
+			if (completeableTasks == null) {
+				completeableTasks = new ArrayList<ICompleteable>();
+			}
+			completeableTasks.add(new CompleteableTask(
+									new SimpleTask(
+											String.valueOf(completeableTasks.size()+1), tasks[i]), 
+									false));
+		}
+	}
+
 	public void runApplication() {
+		runSingleOp(NO_SINGLE_OP);
+	}
+	
+	public void runSingleOp(int singleOp) {
 				
 		startReader();
 		
@@ -54,23 +76,23 @@ public class TaskStoreConsoleApplication {
 		
 		while(!isDone) {
 			
-//			mainPage();
+			mainPage();
 			
-			int selected = readSelect(MAIN_PROMPT);
+			int selected = (singleOp != NO_SINGLE_OP)?singleOp : readSelect(MAIN_PROMPT);
 			switch( selected) {
-			case 1:
+			case ADD_SELECT:
 				addPage();
 				break;
-			case 2:
+			case REMOVE_SELECT:
 				removePage();
 				break;	
-			case 3:
+			case COMPLETE_SELECT:
 				markCompletePage();
 				break;
-			case 4:
+			case LIST_SELECT:
 				listPage();
 				break;
-			case 5:
+			case QUIT_SELECT:
 				quitApplication();
 				isDone = true;
 				break;
@@ -79,86 +101,61 @@ public class TaskStoreConsoleApplication {
 				break;
 			}
 			
+			if (singleOp != NO_SINGLE_OP) {
+				if (singleOp != LIST_SELECT) {
+					listPage();					
+				}
+				quitApplication();
+				isDone = true;
+			}
 		}		
 		done();
 		
 		stopReader();
 	}
-	public void runListPage(String[] tasks) {
-		listPage(tasks);
-		quitApplication();
-		done();
-	}
 
-	public void mainPage() {
+	private void mainPage() {
 		System.out.println(MAIN_PAGE);
 		for(ITaskable task : mainTasks) {
 			System.out.println(task.describe());
 		}
+		System.out.println(MAIN_PROMPT);
 	}
-	public void addPage() {
+	private void addPage() {
 		System.out.println(ADD_PAGE);
 		CompleteableTask newTask = new CompleteableTask("" + getNextTaskNumber(), 
 										readInput(ADD_PROMPT));
 		completeableTasks.add(newTask);
-		listPage();
 	}
-	private CompleteableTask getCompleteableTask(String taskNumber) {
-		int selection = Integer.parseInt(taskNumber);
-		if (completeableTasks != null && completeableTasks.size()>=selection) {
-			for(ITaskable task : completeableTasks) {
-				CompleteableTask ctask = (CompleteableTask)task;
-				if (ctask.getNumber().equals(taskNumber)) {
-					return ctask;
-				}
-			}
-		}
-		return null;
-	}
-	public void removePage() {
+	private void removePage() {
 		System.out.println(REMOVE_PAGE);
-		listPage();
-		String selection = readInput(SELECT_PROMPT);
-		CompleteableTask found = getCompleteableTask(selection);
-		if (found != null) {
-			System.out.println(found.describe() + " is removed from that task list.");
-		}
 		String crLf = readInput(CONTINUE_PROMPT);
 	}
-	public void removePage(String[] tasks) {
-		System.out.println(REMOVE_PAGE);
-		listPage(tasks);
-		String selection = "2";
-		CompleteableTask found = getCompleteableTask(selection);
-		if (found != null) {
-			System.out.println(NEWLINE + NEWLINE + "Removing " + found.describe() + " from that task list.");
-		}
-		String crLf = readInput(CONTINUE_PROMPT);
-	}
-	public void markCompletePage() {
+	private void markCompletePage() {
 		System.out.println(MARK_COMPLETE);
 		String crLf = readInput(CONTINUE_PROMPT);
 	}	
-	public void listPage() {
+	private void listPage() {
 		System.out.println(LIST_PAGE);
 		System.out.print(NEWLINE);
-		for(ITaskable task : completeableTasks) {
-			System.out.println(task.describe());
+		for(ICompleteable task : completeableTasks) {
+			ITaskable ctask = (CompleteableTask)task;
+			System.out.println(ctask.describe());
 		}
 		String crLf = readInput(CONTINUE_PROMPT);
 	}
-	public void listPage(String[] tasks) {
-		for(int i=0; i<tasks.length; i++) {
-			CompleteableTask newTask = new CompleteableTask("" + getNextTaskNumber(), 
-					tasks[i]);
-			completeableTasks.add(newTask);			
-		}
-		listPage();
+	private void choiceNotRecognized() {
+		System.out.println(NOT_RECOGNIZED_PROMPT);
+		String crLf = readInput(CONTINUE_PROMPT);		
 	}
-	public void quitApplication() {
+	private void quitApplication() {
 		System.out.println(QUIT_PROMPT);
 //		String crLf = getUserInput("\r\nPress Any Key to Continue: ");
 	}
+	private void done() {
+		System.out.println("\r\nDONE!");
+	}
+
 
 	// BufferedReader functions.
 	private static void startReader() {
@@ -232,23 +229,28 @@ public class TaskStoreConsoleApplication {
 		}
 		return nextTaskNumber;
 	}
-	private void choiceNotRecognized() {
-		System.out.println(NOT_RECOGNIZED_PROMPT);
-		String crLf = readInput(CONTINUE_PROMPT);		
+	private CompleteableTask getCompleteableTask(String taskNumber) {
+		int selection = Integer.parseInt(taskNumber);
+		if (completeableTasks != null && completeableTasks.size()>=selection) {
+			for(ICompleteable task : completeableTasks) {
+				CompleteableTask ctask = (CompleteableTask)task;
+				if (ctask.getNumber().equals(taskNumber)) {
+					return ctask;
+				}
+			}
+		}
+		return null;
 	}
-	private void done() {
-		System.out.println("\r\nDONE!");
-	}
-
+	
 	public static void main(String[] args) {
 
-		RunTest03();
+		RunTest01();
 	}
 	
 	public static void RunTest01() {
 		
 		TaskStoreConsoleApplication tb = new TaskStoreConsoleApplication();
-		tb.runApplication();
+		tb.runSingleOp(ADD_SELECT);
 	}
 	
 	public static void RunTest02() {
@@ -256,15 +258,10 @@ public class TaskStoreConsoleApplication {
 		String[] tasks = {"Eating a bug", "Learn Commoncore", "Try a Peach", "Have breakfast and coffee"};
 		
 		TaskStoreConsoleApplication tb = new TaskStoreConsoleApplication();
-		tb.runListPage(tasks);
-	}
-	
-	public static void RunTest03() {
+		tb.loadSimpleTasks(tasks);
+		tb.runSingleOp(LIST_SELECT);
 		
-		String[] tasks = {"Eating a bug", "Learn Commoncore", "Try a Peach", "Have breakfast and coffee"};
-		
-		TaskStoreConsoleApplication tb = new TaskStoreConsoleApplication();
-		tb.removePage(tasks);
 	}
+
 
 }
