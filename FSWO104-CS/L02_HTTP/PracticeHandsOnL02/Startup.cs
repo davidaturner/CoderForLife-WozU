637c7e386ad5d07b0864dc13f8ee98a213af2c49
turnin19 <turnin19@earthlink.net>
2020-02-28 19:06:10 -0500

    Finishing up 104CS L02.

--- FSWO104-CS/L02_HTTP/PracticeHandsOnL02/Startup.cs
+++ FSWO104-CS/L02_HTTP/PracticeHandsOnL02/Startup.cs
@@ -17,56 +17,72 @@ namespace PracticeHandsOnL02 {
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure (IApplicationBuilder app, IWebHostEnvironment env) {
             app.Run (async (context) => {
-                string response = "<h1>Query String Parameters</h1>" +
+                string response = "<h1>HTTP Query String Parameters</h1>" +
                     "<p>Enter a URL like:</p>" +
                     "<a href=\"http://localhost:5000/?firstname=Jane&lastname=Smith&age=30\">" +
                     "http://localhost:5000/?firstname=Jane&lastname=Smith&age=30</a>";
 
                 var firstName = "";
+                var defaultFName = "David";
                 var lastName = "";
+                var defaultLName = "Turner";
                 int age = 0;
+                var defaultAge = 27;
+                var doNotDefault = true;
                 foreach (var queryParameter in context.Request.Query) {
                     // response += "<p>" + queryParameter + "</p>";
 
                     if (queryParameter.Key.Equals ("firstname")) {
                         firstName = queryParameter.Value;
-                        response += "<p>" + firstName + "</p>";
+                        if (firstName == null) {
+                            firstName = defaultFName;
+                        }
+                        // response += "<p>" + firstName + "</p>";
                     } else if (queryParameter.Key.Equals ("lastname")) {
                         lastName = queryParameter.Value;
-                        response += "<p>" + lastName + "</p>";
+                        if (lastName == null) {
+                            lastName = defaultLName;
+                        }
+                        // response += "<p>" + lastName + "</p>";
                     } else if (queryParameter.Key.Equals ("age")) {
-                        age = int.Parse (queryParameter.Value);
-                        response += "<p>" + age + "</p>";
+                        String value = queryParameter.Value;
+                        if (value == null) {
+                            age = defaultAge;
+                        } else {
+                            age = int.Parse (value);
+                        }
+                        doNotDefault = false;
+                        // response += "<p>" + age + "</p>";
                     };
                 }
 
-                // if (firstName != "" || lastName != "" || age > 0) {
+                var cookie = context.Request.Cookies["MyCoolLittleCookie"];
 
-                //     var cookie = context.Request.Cookies["MyCoolLittleCookie"];
+                if (!doNotDefault && string.IsNullOrWhiteSpace (cookie)) {
+                    DateTime now = DateTime.Now;
+                    DateTime expires = now + TimeSpan.FromSeconds (15);
+                    String cookieCreated = "Cookie created at: " + now.ToString ("h:mm:ss tt");
+                    if (!doNotDefault) {
+                        cookieCreated += " for " + firstName + " " + lastName + ", " + age + ".";
+                    }
+                    context.Response.Cookies.Append (
+                        "MyCoolLittleCookie",
+                        cookieCreated,
+                        new CookieOptions {
+                            Path = "/",
+                                HttpOnly = false,
+                                Secure = false,
+                                Expires = expires
+                        }
+                    );
 
-                //     if (string.IsNullOrWhiteSpace (cookie)) {
-                //         DateTime now = DateTime.Now;
-                //         DateTime expires = now + TimeSpan.FromSeconds (15);
-                //         context.Response.Cookies.Append (
-                //             "MyCoolLittleCookie",
-                //             "Cookie created at: " +
-                //             now.ToString ("h:mm:ss tt") +
-                //             " for " + firstName + " " + lastName + ", " + age,
-                //             new CookieOptions {
-                //                 Path = "/",
-                //                     HttpOnly = false,
-                //                     Secure = false,
-                //                     Expires = expires
-                //             }
-                //         );
-                //     }
-                // };
+                }
 
-                // response +=
-                //     "<h1>HTTP Cookies</h1>" +
-                //     $"<p>This is the cookie value received from browser: \"<strong>{cookie}</strong>\".</p>" +
-                //     "<p>Refresh page to see current cookie value...</p>" +
-                //     "<p>Cookie expires after 15 seconds.</p>";
+                response +=
+                    "<h1>HTTP Cookies</h1>" +
+                    $"<p>This is the cookie value received from browser: \"<strong>{cookie}</strong>\".</p>" +
+                    "<p>Refresh page to see current cookie value...</p>" +
+                    "<p>Cookie expires after 15 seconds.</p>";
 
                 await context.Response.WriteAsync (response);
             });
