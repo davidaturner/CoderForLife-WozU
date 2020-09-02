5f4c602b52e4ee546f5258bec45c351a84fd8a1b
turnin19 <turnin19@earthlink.net>
2020-09-01 20:20:32 -0400

    More L03 work.

---
+++ FSWO104-JV/STS/L03_Mvc/HandsOnL03/src/main/java/com/handsOn/HandsOnL03Application.java
@@ -0,0 +1,13 @@
+package com.handsOn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandsOnL03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandsOnL03Application.class, args);
+	}
+
+}
---
+++ FSWO104-JV/STS/L03_Mvc/HandsOnL03/src/test/java/com/handsOn/HandsOnL03ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handsOn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandsOnL03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
