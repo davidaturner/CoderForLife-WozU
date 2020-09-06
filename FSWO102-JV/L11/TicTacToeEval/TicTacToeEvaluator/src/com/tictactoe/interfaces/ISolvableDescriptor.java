a1edc82074cd2c7ade37407f200249790d00c606
turnin19 <turnin19@earthlink.net>
2020-09-06 19:53:01 -0400

    Update interfaces for TicTacToe 102JV

--- FSWO102-JV/L11/TicTacToeEval/TicTacToeEvaluator/src/com/tictactoe/interfaces/ISolvableDescriptor.java
+++ FSWO102-JV/L11/TicTacToeEval/TicTacToeEvaluator/src/com/tictactoe/interfaces/ISolvableDescriptor.java
@@ -1,9 +1,11 @@
 package com.tictactoe.interfaces;
 
-public interface ISolvableValidator {
+public interface ISolvableDescriptor {
 
 	public static final String VALID = "Valid";
 	public static final String INVALID = "Invalid";
 	
 	String validate(ISolvable solvable);
+	
+	String[] describe(ISolvable solvable);
 }
--- FSWO102-JV/L11/TicTacToeEval/TicTacToeEvaluator/src/com/tictactoe/interfaces/ISolvableScriptor.java
+++
@@ -1,6 +0,0 @@
-package com.tictactoe.interfaces;
-
-public interface ISolvableScriptor {
-
-	String[] describe(ISolvable solvable);
-}
