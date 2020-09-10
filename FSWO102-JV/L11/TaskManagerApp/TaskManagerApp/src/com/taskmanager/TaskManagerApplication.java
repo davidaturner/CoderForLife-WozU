185442b89cc314faa0b86714502355416b6b1622
turnin19 <turnin19@earthlink.net>
2020-09-09 23:06:08 -0400

    More work on TaskManger.

--- FSWO102-JV/L11/TaskManagerApp/TaskManagerApp/src/com/taskmanager/TaskManagerApplication.java
+++ FSWO102-JV/L11/TaskManagerApp/TaskManagerApp/src/com/taskmanager/TaskManagerApplication.java
@@ -1,15 +1,58 @@
 package com.taskmanager;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+import com.taskmanager.controllers.TaskManagerController;
+import com.taskmanager.factories.CompleteableTaskFactory;
+import com.taskmanager.views.TaskFactoryConsole;
+
 public class TaskManagerApplication {
 
-	public static void run() {
-		System.out.println("Hello, World!");
-	}
+	BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 	
+	static final TaskManagerController controller = new TaskManagerController(
+											new CompleteableTaskFactory(),
+											new TaskFactoryConsole());
+	
+	public void run() {
+		
+		System.out.println("Welcome to Things-To-Do Task Manager.");
+		System.out.println();
+		
+		displayPage(controller.mainpage());
+	}
+
 	public static void main(String[] args) {
 		TaskManagerApplication app = new TaskManagerApplication();
 		app.run();
-
 	}
+	
+	
+	// Support
+	private int getChoice() {
+		try {		
+			return Integer.parseInt(in.readLine());			
+		} catch (Exception e) {
+			System.out.println("Unable to get choice");
+		}
+		return 0;
+	}	
+	private String getInput() {
+		try {
+			return in.readLine();
+		} catch (Exception e) {
+			System.out.println("Unable to get input");
+		}
+		return null;
+	}
+	
+	private static void displayPage(String[] page) {
+		for(int i=1; i<page.length;i++) {
+			System.out.println(page[i]);
+		}
+		System.out.println("\r\n" + page[0]);
+	}
+
 
 }
--- FSWO102-JV/L11/TaskManagerApp/TaskManagerApp/src/com/taskmanager/controllers/TaskManagerController.java
+++ FSWO102-JV/L11/TaskManagerApp/TaskManagerApp/src/com/taskmanager/controllers/TaskManagerController.java
@@ -9,9 +9,9 @@ import com.taskmanager.views.TaskFactoryConsole;
 
 public class TaskManagerController {
 
-	CompleteableTaskFactory factory;
-	TaskFactoryConsole views;
-	
+	TaskFactory factory;
+	ITaskFactoryViews views;
+		
 	public String[] MAIN_PAGE = {
 			"Add a task",
 			"Remove a task",
@@ -21,56 +21,17 @@ public class TaskManagerController {
 	public String MAIN_PROMPT = "What would you like to do?";
 	
 	public TaskManagerController(TaskFactory factory, ITaskFactoryViews views) {
-		this.factory = (CompleteableTaskFactory) factory;
-		this.views = (TaskFactoryConsole) views;
+		this.factory = factory;
+		this.views = views;
 		
-		views.setMainPage((new SimpleTaskFactory()).createAll(MAIN_PAGE), 
-							MAIN_PROMPT);
+		views.setMainPage( (new SimpleTaskFactory()).createAll(MAIN_PAGE), 
+				            MAIN_PROMPT);
 	}
 	
-	// Get
-	public void welcome() {
-		System.out.println("\r\nWelcome to Task Manager!\r\n");
-		mainpage();
-	}
-	public void mainpage() {
-		displayPage( views.getMainPage(), views.getMainPrompt());
-	}
-	public void select(int taskId) {
-	}
-	public void addPage() {
-	}
-	public void removePage() {
-	}
-	public void listPage() {
-	}
-	public void topOfListPage() {
-	}
-	public void nextInListPage() {
-	}
-	public void prevInListPage() {
-	}
-	public void markCompletePage() {
-	}
-	
-	// Post
-	public void addTask(String taskDescription) {
-	}
-	
-	// Delete
-	public void removeTask(int taskId) {
-	}
-	
-	// Put
-	public void markTask(int taskId) {
-	}
-	
-	// Support
-	private void displayPage(ITaskable[]tasks, String prompt) {
-		for(int i=0;i<tasks.length;i++) {
-			System.out.println(tasks[i]);
-		}
-		System.out.println("\r\n" + prompt);
+	public String[] mainpage() {
+		
+		return promptlisting(((TaskFactoryConsole)views).getMainPage(), 
+							((TaskFactoryConsole)views).getMainPrompt());
 	}
 	
 	public static void main(String[] args) {
@@ -78,8 +39,23 @@ public class TaskManagerController {
 		TaskManagerController controller = new TaskManagerController(
 												new CompleteableTaskFactory(),
 												new TaskFactoryConsole());
-		controller.welcome();
-		
+		displayPage(controller.mainpage());
+	}
+	
+	private String[] promptlisting(ITaskable[] tasks, String prompt) {
+		String[] page = new String[tasks.length+1];
+		page[0] = prompt;
+		for(int i=1; i<tasks.length+1; i++) {
+			page[i] = tasks[i-1].toString();
+		}
+		return page;
+	}
+	
+	private static void displayPage(String[] page) {
+		for(int i=1; i<page.length;i++) {
+			System.out.println(page[i]);
+		}
+		System.out.println("\r\n" + page[0]);
 	}
 
 }
