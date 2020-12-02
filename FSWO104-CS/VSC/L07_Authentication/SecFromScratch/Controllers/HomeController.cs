494bfbd708be236ed263206f09daa92ce6f4d57e
turnin19 <turnin19@earthlink.net>
2020-12-02 08:38:14 -0500

    Fixed 104CS L07 proj

--- FSWO104-CS/VSC/L07_Authentication/SecFromScratch/Controllers/HomeController.cs
+++ FSWO104-CS/VSC/L07_Authentication/SecFromScratch/Controllers/HomeController.cs
@@ -4,59 +4,85 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using SecFromScratch.Models;
+using SecFromScratch.Services;
 
-namespace SecFromScratch.Controllers
-{
-    public class HomeController : Controller
-    {
+namespace SecFromScratch.Controllers {
+    public class HomeController : Controller {
         private readonly ILogger<HomeController> _logger;
         private readonly AppDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger, AppDbContext context)
-        {
+        public HomeController (ILogger<HomeController> logger, AppDbContext context) {
             _logger = logger;
             _context = context;
         }
 
-        public IActionResult Index()
-        {
-            return View();
+        public IActionResult Index () {
+            return View ();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
+        public IActionResult Privacy () {
+            return View ();
+        }
+
+        [HttpGet]
+        public IActionResult Login () {
+            return View ();
+        }
+
+        [HttpGet]
+        public IActionResult Treats () {
+            return View ();
+        }
+
+        [HttpGet]
+        public IActionResult LoginFail () {
+            return View ();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(User user) {
+        public async Task<IActionResult> Create (User user) {
 
             if (ModelState.IsValid) {
-
-                _context.Add(user);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Welcome));
+                // Hash the password
+                user.Password = ManualAuth.Sha256 (user.Password);
+                // Now add.
+                _context.Add (user);
+                await _context.SaveChangesAsync ();
+                return RedirectToAction (nameof (Welcome));
             }
-            return View(user);
+            return View (user);
         }
 
-        public IActionResult Welcome()
-        {
-            return View();
+        public IActionResult Welcome () {
+            return View ();
         }
 
-        public ViewResult Login()
-        {
-            return View();
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Verify (User user) {
+
+            if (ModelState.IsValid) {
+                // attempt to get a user with the matching username from DB.
+                User GetUser = await _context.Users.SingleOrDefaultAsync (u => u.UserName == user.UserName);
+                // if no match on username skip password check.
+                if (GetUser != null) {
+                    // compare hashed passwords.
+                    if (ManualAuth.Sha256Check (user.Password, GetUser.Password)) {
+                        // if password match is true return treats.
+                        return RedirectToAction (nameof (Treats));
+                    }
+                }
+            }
+            return RedirectToAction (nameof (LoginFail));
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        [ResponseCache (Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error () {
+            return View (new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
--- FSWO104-CS/VSC/L07_Authentication/SecFromScratch/Services/ManualAuth.cs
+++ FSWO104-CS/VSC/L07_Authentication/SecFromScratch/Services/ManualAuth.cs
@@ -1,19 +1,22 @@
 using System.Security.Cryptography;
 using System.Text;
 
-namespace SecFromScratch.Services
-{
-    public class ManualAuth
-    {
-        public static string Sha256(string randomstring) {
-            System.Security.Cryptography.SHA256Managed crypt = new SHA256Managed();
-            StringBuilder sb = new StringBuilder();
-            byte[] crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(randomstring), 0, 
-                                Encoding.UTF8.GetByteCount(randomstring));
-            foreach(byte theByte in crypto) {
-                sb.Append(theByte.ToString("x2"));
+namespace SecFromScratch.Services {
+    public class ManualAuth {
+        public static string Sha256 (string randomstring) {
+            System.Security.Cryptography.SHA256Managed crypt = new SHA256Managed ();
+            StringBuilder sb = new StringBuilder ();
+            byte[] crypto = crypt.ComputeHash (Encoding.UTF8.GetBytes (randomstring), 0,
+                Encoding.UTF8.GetByteCount (randomstring));
+            foreach (byte theByte in crypto) {
+                sb.Append (theByte.ToString ("x2"));
             };
-            return sb.ToString();
+            return sb.ToString ();
+        }
+
+        public static bool Sha256Check (string userInput, string valueToCompare) {
+            string sb = ManualAuth.Sha256 (userInput);
+            return (valueToCompare == sb);
         }
     }
 }
